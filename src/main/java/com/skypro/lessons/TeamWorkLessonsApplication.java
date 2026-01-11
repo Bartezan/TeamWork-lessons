c39c9029c29de58f38f8b4cec88352ad210b9c46
Bartezan <Bartez2202@gmail.com>
2026-01-11 14:15:03 +1000

    init commit

---
+++ src/main/java/com/skypro/lessons/TeamWorkLessonsApplication.java
@@ -0,0 +1,14 @@
+package com.skypro.lessons;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@OpenAPIDefinition
+@SpringBootApplication
+public class TeamWorkLessonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamWorkLessonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/lessons/TeamWorkLessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.lessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamWorkLessonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
