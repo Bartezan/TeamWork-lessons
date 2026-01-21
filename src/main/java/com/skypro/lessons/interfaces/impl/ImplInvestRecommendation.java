a97a06f1e6084fea958d833a8e813966a775f6cc
Bartezan <Bartez2202@gmail.com>
2026-01-21 21:24:18 +1000

    develop : code refactoring was performed, add enum ProdType and TransactionType

--- src/main/java/com/skypro/lessons/interfaces/impl/ImplInvestRecommendation.java
+++ src/main/java/com/skypro/lessons/interfaces/impl/ImplInvestRecommendation.java
@@ -1,5 +1,7 @@
 package com.skypro.lessons.interfaces.impl;
 
+import com.skypro.lessons.entity.ProductType;
+import com.skypro.lessons.entity.TransactionType;
 import com.skypro.lessons.interfaces.RecommendationRuleSet;
 import com.skypro.lessons.models.RecomendationDto;
 import com.skypro.lessons.repositories.RecommendationsRepository;
@@ -16,30 +18,16 @@ public class ImplInvestRecommendation implements RecommendationRuleSet {
 
     @Override
     public Optional<RecomendationDto> getRecommendation(UUID user) {
-        int countDebit = repository.getIntFromCustomRequest("""
-                SELECT COUNT(*)
-                FROM TRANSACTIONS AS t
-                JOIN USERS u ON t.USER_ID =u.ID
-                JOIN PRODUCTS p ON t.PRODUCT_ID =p.ID""" +
-                " WHERE t.USER_ID ='" + user + "' AND p.TYPE = 'DEBIT'");
-        int countInvest = repository.getIntFromCustomRequest("""
-                SELECT COUNT(*)
-                FROM TRANSACTIONS AS t
-                JOIN USERS u ON t.USER_ID =u.ID
-                JOIN PRODUCTS p ON t.PRODUCT_ID =p.ID""" +
-                " WHERE t.USER_ID ='" + user + "' AND p.TYPE = 'INVEST'");
-        int sumSavingDeposit = repository.getIntFromCustomRequest("""
-                SELECT  SUM(t.AMOUNT)
-                FROM TRANSACTIONS AS t
-                JOIN USERS u ON t.USER_ID =u.ID
-                JOIN PRODUCTS p ON t.PRODUCT_ID =p.ID""" +
-                " WHERE t.USER_ID ='" + user + "' AND p.TYPE = 'SAVING' AND t.TYPE = 'DEPOSIT'");
-        if (countDebit >= 1 && countInvest == 0 && sumSavingDeposit > 1000) {
-            System.out.println("Invest true");
+        boolean hasDebit = repository.hasProduct(user, ProductType.DEBIT.name());
+
+        boolean hasInvest = repository.hasProduct(user, ProductType.INVEST.name());
+
+        int sumSavingDeposit = repository.getSumByProdTypeAndTransactionsType(user, ProductType.SAVING.name(), TransactionType.DEPOSIT.name());
+
+        if (hasDebit && !hasInvest && sumSavingDeposit > 1000) {
             RecomendationDto investRecommendation = new RecomendationDto("Invest 500", UUID.fromString("147f6a0f-3b91-413b-ab99-87f081d60d5a"), "Откройте свой путь к успеху с индивидуальным инвестиционным счетом (ИИС) от нашего банка! Воспользуйтесь налоговыми льготами и начните инвестировать с умом. Пополните счет до конца года и получите выгоду в виде вычета на взнос в следующем налоговом периоде. Не упустите возможность разнообразить свой портфель, снизить риски и следить за актуальными рыночными тенденциями. Откройте ИИС сегодня и станьте ближе к финансовой независимости!");
             return Optional.of(investRecommendation);
         }
-        System.out.println("Invest false");
         return Optional.empty();
     }
 }
